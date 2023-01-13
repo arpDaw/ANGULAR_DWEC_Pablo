91f7185165ed4f9fd568cc98ebaad6b3dc64834e
Pablo <pabloruizalfaro@gmail.com>
2023-01-13 09:24:39 +0100

    componentes agregar y personajes

--- helloworld/src/app/dbz/dbz.module.ts
+++ helloworld/src/app/dbz/dbz.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { FormsModule } from '@angular/forms';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 
 
 @NgModule({
   declarations: [
-    MainpageComponent
+    MainpageComponent,
+    PersonajesComponent,
+    AgregarComponent
   ],
   imports: [
     CommonModule,
--- helloworld/src/app/dbz/mainpage/mainpage.component.ts
+++ helloworld/src/app/dbz/mainpage/mainpage.component.ts
@@ -1,9 +1,5 @@
-import { Component } from '@angular/core';
-
-interface Personaje{
-  nombre: string
-  poder: number
-}
+import { Component, Input } from '@angular/core';
+import {Personaje} from   '../interfaces/dbz.interfaces'
 
 
 
@@ -14,6 +10,7 @@ interface Personaje{
 })
 export class MainpageComponent {
 
+
 personajes: Personaje[] = [
   {nombre:'Goku', poder:20000},
   {nombre:'Vegeta', poder:10000},
@@ -21,22 +18,5 @@ personajes: Personaje[] = [
   {nombre:'Satán', poder:5}
 ]
 
-nuevo: Personaje = {
-  nombre: "Trunks",
-  poder: 12000
-}
 
-
-
-agregar(){
-
-  if (this.nuevo.nombre.trim().length === 0){
-    return
-  }
-  this.personajes.push(this.nuevo)
-  this.nuevo = {
-    nombre: "",
-    poder: 0
-  }
-}
 }
