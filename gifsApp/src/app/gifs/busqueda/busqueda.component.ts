4312c417ee6c05f4b1df2ae772846a465c892800
Pablo <pabloruizalfaro@gmail.com>
2023-01-24 08:12:45 +0100

    ea

---
+++ gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.scss']
+})
+export class BusquedaComponent {
+
+@ViewChild("txtBuscar") txtBuscar!:ElementRef<HTMLInputElement>;
+
+constructor(private GifsService: GifsService){}
+
+buscar(){
+  console.log(this.txtBuscar)
+
+  const valor = this.txtBuscar.nativeElement.value
+  console.log(valor)
+
+  this.GifsService.buscarGifs(valor)
+
+
+  this.txtBuscar.nativeElement.value ="";
+}
+
+}
